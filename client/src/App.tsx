f552eafc591d1ea15205d842963fb1ff80282843
Steven <digitalexile99@gmail.com>
2023-10-25 21:47:55 -0600

    finished auth page and forms as well as refactoring app component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 import UserAuthPage from "./components/UserAuthPage";
+import DashboardLayout from "./components/Dashboard/DashboardLayout";
+
+interface AuthPageWrapperProps {
+  isSignup: boolean;
+}
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/sign-up" element={<UserAuthPage />} />
+        <Route path="/" element={<DashboardLayout />} />
+        <Route path="/sign-up" element={<AuthPageWrapper isSignup={true} />} />
+        <Route path="/login" element={<AuthPageWrapper isSignup={false} />} />
       </Routes>
     </>
   );
+}
+
+function AuthPageWrapper({ isSignup }: AuthPageWrapperProps) {
+  return <UserAuthPage signup={isSignup} />;
 }
 
 export default App;
