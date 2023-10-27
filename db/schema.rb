13d5f0dac6f2fffc7a38f8801461be2a7be6fe26
Kyle <kyle@kyleo.dev>
2023-10-26 20:54:16 -0500

    updated database.yaml for pg. Added scaffold for lists

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_10_26_024408) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "boards", force: :cascade do |t|
     t.string "title", null: false
     t.integer "user_id", null: false
