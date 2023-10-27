9a90d3391cc1df4d5902c1aa9f8aa7d676fdd47f
Kyle <kyle@kyleo.dev>
2023-10-26 21:20:53 -0500

    added acts as list gem and started setting up relationships

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,4 @@
 class List < ApplicationRecord
+     acts_as_list
+     belongs_to :board
 end
--- db/migrate/20231027015346_create_lists.rb
+++ db/migrate/20231027015346_create_lists.rb
@@ -1,7 +1,9 @@
 class CreateLists < ActiveRecord::Migration[7.0]
   def change
     create_table :lists do |t|
-
+      t.string :name
+      t.integer :position
+      t.integer :board_id
       t.timestamps
     end
   end
