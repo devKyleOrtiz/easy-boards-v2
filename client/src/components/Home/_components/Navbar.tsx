a01dd4e4c0675d00e67f756292e09d2fc710a84c
Steven <digitalexile99@gmail.com>
2023-11-01 15:51:34 -0600

    conditional rendering for navbar

--- client/src/components/Home/_components/Navbar.tsx
+++ client/src/components/Home/_components/Navbar.tsx
@@ -2,10 +2,19 @@ import React from "react";
 import Logo from "../../../assets/logo.svg?react";
 import HoverComponents from "./HoverComponents";
 import { ThemeToggle } from "./ThemeToggle";
+import { useTheme } from "@/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 export default function Navbar() {
+  const { theme } = useTheme();
+
   return (
-    <div className="w-screen h-14 flex justify-around items-center bg-background">
+    <div
+      className={cn("w-screen h-14 flex justify-around items-center", {
+        "bg-primary": theme === "light",
+        "bg-background": theme === "dark",
+      })}
+    >
       <Logo />
       <HoverComponents />
       <ThemeToggle />
